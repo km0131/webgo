0df565139e333eabdddda74bf58e7286f4744074
kaito <kaito013126@gmail.com>
2025-10-20 07:05:59 +0000

    ログイン画面

--- main.go
+++ main.go
@@ -14,5 +14,11 @@ func main() {
 			"title": "ログイン画面",
 		})
 	})
+
+	r.GET("/signup", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "signup.html", gin.H{
+			"title": "新規登録画面",
+		})
+	})
 	r.Run(":8080")
 }
